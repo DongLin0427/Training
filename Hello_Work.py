3861dca9014d75c83f2fe533ecdf97e26ce7b0ea
DongLin0427 <103050967+DongLin0427@users.noreply.github.com>
2022-05-21 10:41:23 +0800

    X1.0.0

---
+++ Hello_Work.py
@@ -0,0 +1,18 @@
+#Today is 28Feb2022
+print("Hello Python")
+#有順序、可動的列表 List
+[3,4,5]
+["Hello","World"]
+#有順序、不可動的列表 Tuple
+(3,4,5)
+("Hello","World")
+#集合 Set
+{3,4,5}
+{"Hello","World"}
+#字典 Dictionary
+{"apple":"蘋果","data":"資料"}
+#變數：用來儲存資料的自訂名稱
+#變數名稱=資料
+x=3
+#print(資料)
+print(x)
